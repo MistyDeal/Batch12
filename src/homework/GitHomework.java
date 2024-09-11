e074e1e9e0a13fc506de2729c8cc4fdd43c01a54
misty <misty@MistysLaptop.lan>
2024-09-10 18:15:07 -0700

    deleted methods

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,33 +2,5 @@ package homework;
 
 public class GitHomework {
 
-	public static void main(String[] args) {
-	System.out.println("Batch 12: Git Class Lecture");
-	int age = 12;
-	String name = "John";
-	//This is coming from GitHub	
 	
-	}
-	public static void add(int a, int b) {
-		System.out.println(a+b);
-		char grade = 'A';
-		int raise = 20;
-		String symptom = "headache";
-	
-	
-
-	}
-	public static void abc(char a, char b){
-		System.out.println(a+b);
-		String frustration = "ex-husband";
-		int years = 12;
-		String solution = "divorce";
-	}
-	public static void divide(int a, int b) {
-		System.out.println(a/b);
-	}
-	public static void minus(int a, int b) {
-		System.out.println(b - a);
-	}
-
 }
