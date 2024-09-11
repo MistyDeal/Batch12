c9ebeafa9b59f831f316407eb76b0cbd2ac810be
misty <misty@MistysLaptop.lan>
2024-09-10 17:36:03 -0700

    added new method

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -4,6 +4,12 @@ public class GitHomework {
 
 	public static void main(String[] args) {
 	System.out.println("Batch 12: Git Class Lecture");
+	
+	}
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	
+	
 
 	}
 
